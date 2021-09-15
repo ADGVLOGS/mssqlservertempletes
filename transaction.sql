5f2eca8985d59be8bcec259dd8ed8a0d27e8d958
Ashlin Darius Govindasamy <adgrules@hotmail.com>
2021-09-16 00:16:15 +0200

    Update transaction.sql

--- transaction.sql
+++ transaction.sql
@@ -1,3 +1,4 @@
+/****** Object:  StoredProcedure ADGTranactionTemplete ******/
 USE [db]
 GO
 SET ANSI_NULLS ON
@@ -5,13 +6,12 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE [dbo].[tblAddress] @personid varchar(max), @Address_Type text ,@Address_Line1 text,@Address_Line2 text , @Address_Line3 text, @Suburb text, @Town text, @City text, @Province text, @Postal_Code smallint
-AS
+CREATE PROCEDURE TransactionDemo
 BEGIN	 
    SET NOCOUNT ON;
 BEGIN TRY
     BEGIN TRANSACTION
-		INSERT INTO [dbo].[tblAddress_Data]([personid],[Address_Type],[Address_Line1],[Address_Line2],[Address_Line3],[Suburb],[City],[Province],[Postal_Code]) VALUES (@personid,@Address_Type,@Address_Line1,@Address_Line3,@Suburb,@Town,@City,@Province,@Postal_Code)
+-- DO WHAT YOU WANT HERE ==========================================================================
     COMMIT TRAN 
 END TRY
 BEGIN CATCH
